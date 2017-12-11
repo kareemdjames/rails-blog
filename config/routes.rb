04c57abdf3515374317b109bbf931e94a5c3745e
Kareem James <kareemdjames@gmail.com>
2017-12-10 20:31:37 -0500

    Comments are working and displayed on the show page, delete button is created but not hooked up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  resources :posts do
+    #nested because they are within the scope of posts comments belong to posts
+    resources :comments
+  end
 
   root 'posts#index'
 
